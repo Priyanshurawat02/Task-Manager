5b6de59408175be813ead529b845d63c7f78630e
Priyanshu rawat <priyanshurawat0311@gmail.com>
2026-03-09 22:36:55 +0530

    loacl storage added

--- src/TaskManager.tsx
+++ src/TaskManager.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 interface Task {
     title: string;
@@ -8,8 +8,10 @@ interface Task {
 
 export default function TaskManager() {
     const[tasks, setTasks] =useState<Array<Task>>([]);
-
     const [inputValue, setInputValue] =useState<string>("");
+    const [hydrated, setHydrated] =useState(false);
+    const[filter,setFilter] =useState<"all" | "active" | "completed">("all");
+
 
    const addTask=()=> {
     if(!inputValue.trim()) return;
@@ -23,24 +25,49 @@ export default function TaskManager() {
     setInputValue("");
   }
 
+   useEffect(()=>{
+    const savedTasks= localStorage.getItem("tasks")
+    if(savedTasks){
+        setTasks(JSON.parse(savedTasks))
+    }
+    setHydrated(true);
+  },[])
+
+  useEffect(()=>{
+    if(!hydrated) return;
+    localStorage.setItem("tasks",JSON.stringify(tasks))
+  },[tasks,hydrated])
+
+ 
+
 const deleteTask=(id:number)=>{
     const newTasks = tasks.filter((task) =>task.id !== id);
     setTasks(newTasks);
 }
 
-const toggleTask=(id:number) =>{
-    const newTasks= tasks.map((task)=>{
-        if(task.id ===id){
+const toggleTask =(id:number) =>{
+    const newToggle = tasks.map((task)=>{
+        if(task.id === id){
             return {
                 ...task,
-                isCompleted:!task.isCompleted
+                isCompleted: !task.isCompleted
             };
-        }else{
-            return task;
-        }
+        } else{
+                return task;
+            }      
     });
-        setTasks(newTasks);
+     setTasks(newToggle);
+}
+
+const filteredTask=tasks.filter(task =>{
+    if (filter==="active"){
+    return !task.isCompleted
+    } if(filter === "completed"){
+        return task.isCompleted
     }
+    return true;
+})
+  
 
 
     return (
@@ -48,16 +75,26 @@ const toggleTask=(id:number) =>{
            <input 
            value={inputValue}
            onChange={(e)=>setInputValue(e.target.value)}/>
-           {tasks.map((task) =>(
+
+           <button onClick={addTask} disabled={!inputValue.trim()}>Add</button>
+
+           <div>
+              <button onClick={() => setFilter("all")}>All</button>
+              <button onClick={() => setFilter("active")}>Active</button>
+              <button onClick={() => setFilter("completed")}>Completed</button>
+            </div>
+
+           {filteredTask.map((task) =>(
                 <div key={task.id}>
-                <p onClick={()=> toggleTask(task.id)}
-                    style={{textDecoration: task.isCompleted ? "line-through":"none"}}>{task.title}</p>
+                <p 
+                onClick={()=> toggleTask(task.id)}
+                    style={{textDecoration: task.isCompleted ? "line-through": "none"}}>
+                        {task.title}</p>
+
                 <button onClick={()=>deleteTask(task.id)}>Delete</button>
                 </div>
-            ))}
-        
-            <button onClick={addTask} disabled={!inputValue.trim()}>Add</button>
+            ))} 
              
         </div>
     )
-}
+}   
